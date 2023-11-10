925b1931530f7ce826c4e4b68ea9bd4f57039a29
MihailPaduraru <mch.mihaii@yahoo.com>
2023-11-10 22:33:13 +0000

    Exercise 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,37 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, '0')}`;
+}
 
+// Obținem referințe către butoane și corpul documentului
+const startButton = document.querySelector('[data-start]');
+const stopButton = document.querySelector('[data-stop]');
+const body = document.body;
+
+let intervalId;
+
+// Adăugăm ascultători de eveniment pentru butoane
+startButton.addEventListener('click', startColorSwitch);
+stopButton.addEventListener('click', stopColorSwitch);
+
+// Funcția pentru a genera o culoare aleatorie și seta culoarea de fundal a corpului
+function changeBackgroundColor() {
+  body.style.backgroundColor = getRandomHexColor();
+}
+
+// Funcția pentru a începe schimbarea culorii de fundal la fiecare secundă
+function startColorSwitch() {
+  startButton.disabled = true; // Dezactivăm butonul "Start"
+  stopButton.disabled = false; // Activăm butonul "Stop"
+
+  intervalId = setInterval(changeBackgroundColor, 1000);
+}
+
+// Funcția pentru a opri schimbarea culorii de fundal
+function stopColorSwitch() {
+  startButton.disabled = false; // Activăm din nou butonul "Start"
+  stopButton.disabled = true; // Dezactivăm butonul "Stop"
+
+  clearInterval(intervalId);
+}
